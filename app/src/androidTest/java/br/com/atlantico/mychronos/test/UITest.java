6443d9d06ca8f153782e49f2108596f5044bd9a4
Nascimentokp <nascimentokp@gmail.com>
2015-06-22 16:18:26 -0300

    Alterado novas funcionalidades

--- app/src/androidTest/java/br/com/atlantico/mychronos/test/UITest.java
+++ app/src/androidTest/java/br/com/atlantico/mychronos/test/UITest.java
@@ -1,17 +1,25 @@
 package br.com.atlantico.mychronos.test;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.RenamingDelegatingContext;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.robotium.solo.Solo;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.atlantico.mychronos.R;
 import br.com.atlantico.mychronos.activities.MainActivity;
+import br.com.atlantico.mychronos.db.ChronosDBHelper;
+import br.com.atlantico.mychronos.db.TimestampEntry;
 
 import static br.com.atlantico.mychronos.R.id.txtSecondOut;
 
@@ -24,16 +32,23 @@ public class UITest extends ActivityInstrumentationTestCase2<MainActivity>{
 
     public UITest() {
         super(MainActivity.class);
+
     }
 
     @Override
     public void setUp() throws Exception {
+        super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
+        ChronosDBHelper helper = new ChronosDBHelper(getActivity());
+        helper.onUpgrade(helper.getWritableDatabase(), 1, 1);
     }
 
     @Override
     public void tearDown() throws Exception {
+        ChronosDBHelper helper = new ChronosDBHelper(getActivity());
+        helper.onUpgrade(helper.getWritableDatabase(), 1, 1);
        solo.finishOpenedActivities();
+        super.tearDown();
     }
 
     //Verificando se está na tela principal
@@ -58,11 +73,23 @@ public class UITest extends ActivityInstrumentationTestCase2<MainActivity>{
     //Selecionando menu Lateral
     public void testMenuLateral(){
         solo.clickOnActionBarHomeButton();
-        boolean lateral = solo.searchText("Ponto") && solo.searchText("Tarefas") && solo.searchText("Relatório") && solo.searchText("Configurações") && solo.searchText("Sobre");
-        assertTrue("Falta algum menu lateral", lateral);
+        Assert.assertTrue(solo.searchText("Ponto"));
+        Assert.assertTrue(solo.searchText("Tarefas"));
+        Assert.assertTrue(solo.searchText("Relatório"));
+        Assert.assertTrue(solo.searchText("Configurações"));
+        Assert.assertTrue(solo.searchText("Sobre"));
         solo.clickOnText("Sobre");
 
 
+    }
+
+    public void testBtnCancelar(){
+        solo.clickOnText("Ponto");
+        solo.clickOnView(solo.getView(R.id.btnCheck));
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("OK"));
+        Assert.assertTrue(solo.searchText("Cancelar"));
+        solo.clickOnButton("Cancelar");
     }
 
     //Teste clicando no botão de bater ponto
@@ -71,9 +98,10 @@ public class UITest extends ActivityInstrumentationTestCase2<MainActivity>{
         solo.clickOnView(solo.getView(R.id.btnCheck));
         solo.setTimePicker(0, 8, 01);
         solo.waitForDialogToOpen();
-        assertTrue(solo.searchButton("OK") && solo.searchButton("Cancelar")); ;
+        Assert.assertTrue(solo.searchText("OK"));
+        Assert.assertTrue(solo.searchText("Cancelar"));
         solo.clickOnButton("OK");
-        solo.clickOnView(solo.getView(R.id.btnCheck));
+        solo.clickOnButton("Bater Ponto");
         solo.setTimePicker(0, 11, 30);
         solo.clickOnButton("OK");
         solo.clickOnView(solo.getView(R.id.btnCheck));
@@ -84,8 +112,7 @@ public class UITest extends ActivityInstrumentationTestCase2<MainActivity>{
         solo.clickOnButton("OK");
         solo.waitForDialogToClose();
         //Verificando se a hora trabalhada é igual a esperada.
-        boolean campos = solo.searchText("Horas trabalhadas:") && (((TextView)  solo.getView(R.id.txtWorkedTime)).getText()).equals("8h");
-        assertTrue("Não encontrada a mensagem, Horas trabalhadas",campos);
+        Assert.assertTrue("Horas trabalhadas: 8h", (((TextView) solo.getView(R.id.txtWorkedTime)).getText()).equals("8h"));
         solo.takeScreenshot("Ponto-horas_trabalhadas");
 
     }
@@ -94,34 +121,27 @@ public class UITest extends ActivityInstrumentationTestCase2<MainActivity>{
     public void testPrevisaoSaida() {
         solo.clickOnText("Ponto");
         solo.clickOnView(solo.getView(R.id.btnCheck));
-        solo.setTimePicker(0, 8, 00);
         solo.waitForDialogToOpen();
-        assertTrue(solo.searchButton("OK") && solo.searchButton("Cancelar"));
+        Assert.assertTrue(solo.searchText("OK"));
+        Assert.assertTrue(solo.searchText("Cancelar"));
+        //Apenas testando o botão Cancelar
+        solo.clickOnButton("Cancelar");
+        //Continuando...
+        solo.clickOnView(solo.getView(R.id.btnCheck));
+        solo.setTimePicker(0, 8, 00);
+        solo.clickOnButton("OK");
+        solo.clickOnButton("Bater Ponto");
+        solo.setTimePicker(0, 11, 30);
         solo.clickOnButton("OK");
         solo.clickOnView(solo.getView(R.id.btnCheck));
-        solo.setTimePicker(0, 12, 00);
-        solo.clickOnButton("OK");
-        solo.clickOnView(solo.getView(R.id.btnCheck));
-        solo.setTimePicker(0, 13, 00);
+        solo.setTimePicker(0, 12, 30);
         solo.clickOnButton("OK");
         solo.clickOnView(solo.getView(R.id.btnCheck));
         solo.setTimePicker(0, 17, 00);
         solo.clickOnButton("OK");
         solo.waitForDialogToClose();
-        boolean campos = solo.searchText("Previsão de Saída:") && (((TextView)  solo.getView(R.id.txtTimToLeave)).getText()).equals("17:00");
-        assertTrue("Hora de saída não é igual a hora esperada",campos);
+        Assert.assertTrue("Previsão de Saída: 17:00", (((TextView) solo.getView(R.id.txtTimToLeave)).getText()).equals("17:00"));
         solo.takeScreenshot("Ponto-previsao_saida");
-
-    }
-
-    //Testando o botão cancelar do dialog
-    public void testBotaoCancelar(){
-        solo.clickOnText("Ponto");
-        solo.clickOnView(solo.getView(R.id.btnCheck));
-        solo.waitForDialogToOpen();
-        assertTrue(solo.searchButton("OK") && solo.searchButton("Cancelar")); ;
-        solo.clickOnButton("Cancelar");
-        solo.waitForDialogToClose();
 
     }
 
