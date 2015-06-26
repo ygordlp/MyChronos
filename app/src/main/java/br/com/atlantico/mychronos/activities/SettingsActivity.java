ec93dc616088c3efef18d00961f23de4b2201b09
leonardo_nogueira <leonardo_nogueira@atlantico.com.br>
2015-06-26 15:41:11 -0300

    Settings implementation

--- app/src/main/java/br/com/atlantico/mychronos/activities/SettingsActivity.java
+++ app/src/main/java/br/com/atlantico/mychronos/activities/SettingsActivity.java
@@ -1,25 +1,84 @@
+
 package br.com.atlantico.mychronos.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import br.com.atlantico.mychronos.R;
+import br.com.atlantico.mychronos.model.Constants;
 
 /**
- * Created by pereira_ygor on 03/06/2015.
+ * Settings activity
+ * 
+ * @author pereira_ygor
+ * @since 6/3/2015
  */
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements
+        RadioGroup.OnCheckedChangeListener {
+
+    private SharedPreferences mPrefs;
+
+    private SharedPreferences.Editor mEditor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.settings_rg_journey);
+        RadioButton rbOp4 = (RadioButton)findViewById(R.id.settings_rb_op4);
+        RadioButton rbOp6 = (RadioButton)findViewById(R.id.settings_rb_op6);
+        RadioButton rbOp8 = (RadioButton)findViewById(R.id.settings_rb_op8);
+
+        mPrefs = getSharedPreferences(Constants.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+
+        switch (mPrefs.getInt(Constants.SHARED_PREF_DAILY_JOURNEY, 8)) {
+            case 4: {
+                rbOp4.setChecked(true);
+                break;
+            }
+            case 6: {
+                rbOp6.setChecked(true);
+                break;
+            }
+            default: {
+                rbOp8.setChecked(true);
+                break;
+            }
+        }
+
+        radioGroup.setOnCheckedChangeListener(this);
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+        if (mEditor == null) {
+            mEditor = mPrefs.edit();
+        }
+
+        mEditor.putInt(Constants.SHARED_PREF_DAILY_JOURNEY,
+                Integer.valueOf(findViewById(checkedId).getTag().toString()));
+
+        mEditor.apply();
 
     }
 }
--- app/src/main/java/br/com/atlantico/mychronos/model/Constants.java
+++ app/src/main/java/br/com/atlantico/mychronos/model/Constants.java
@@ -1,10 +1,18 @@
+
 package br.com.atlantico.mychronos.model;
 
 /**
- * Created by pereira_ygor on 25/06/2015.
+ * Application Constants
+ * 
+ * @author pereira_ygor
+ * @since 6/25/2015
  */
 public interface Constants {
 
-    public static final String SHARED_PREFERENCES = "MY_CHRONOS_SHARED_PREFERENCES";
-    public static final String SHARED_PREF_LAST_TASK = "SHARED_PREF_LAST_TASK";
+    String SHARED_PREFERENCES = "MY_CHRONOS_SHARED_PREFERENCES";
+
+    String SHARED_PREF_LAST_TASK = "SHARED_PREF_LAST_TASK";
+
+    String SHARED_PREF_DAILY_JOURNEY = "SHARED_PREF_DAILY_JOURNEY";
+
 }
